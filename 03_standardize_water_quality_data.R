b11c50b11b71f59d55763ab550de7ed101e0aca4
IASTATE\gio <giorgichi@gmail.com>
2019-10-02 15:17:22 -0500

    Add water sample type
     - Specified sample type of water quality data for all sites
    
    Resolves: Grab vs ISCO samples #isudatateam/datateam/issues/184

--- 03_standardize_water_quality_data.R
+++ 03_standardize_water_quality_data.R
@@ -12,22 +12,29 @@ wq_ALL <- read_rds('Inter_Data/wq_ALL.rds')
 wq_ALL %>%
   # add sample type based on TD Sampling Method data
   mutate(sample_type = case_when(siteid == 'ACRE' & date < ymd(20160301) ~ 'Grab',
-                                 siteid == 'ACRE' & date > ymd(20160229) ~ 'Time Proportional', # ISCO
+                                 siteid == 'ACRE' & date > ymd(20160229) ~ 'Time Proportional',     # ISCO
+                                 siteid == 'DPAC' ~ 'Time Proportional',                            # ISCO
+                                  # some samples at DPAC were grab during the winter but we do not know which
+                                 siteid %in% c('AUGLA', 'CRAWF', 'DEFI_M', 'HARDIN') ~ 'Grab',
                                  siteid %in% c('BEAR', 'BEAR2', 'BENTON', 'DIKE', 
                                                'HICKORY', 'MAASS', 'SHEARER') ~ 'Grab',
                                  siteid == 'HICKS_B' & sample_type == 'ISCO' ~ 'Flow Proportional', # ISCO
                                  siteid == 'STORY' ~ 'Flow Proportional',
-                                 siteid %in% c('MUDS2', 'MUDS4') ~ 'Time Proportional',         # ISCO
-                                 siteid %in% c('MUDS3_OLD', 'MUDS3_NEW') ~ 'Time Proportional', # Sigma
+                                 siteid %in% c('MUDS2', 'MUDS4') ~ 'Time Proportional',             # ISCO
+                                 siteid %in% c('MUDS3_OLD', 'MUDS3_NEW') ~ 'Time Proportional',     # Sigma
+                                 siteid == 'DEFI_R' & str_detect(location, 'lysimeter') ~ 'Grab',
                                  siteid == 'CLAY_C' ~ 'Grab',
                                  siteid == 'CLAY_R' ~ 'Grab',
                                  siteid == 'FAIRM'  ~ 'Grab',
                                  siteid == 'SERF_IA' ~ 'Grab',
                                  siteid == 'SERF_SD' ~ 'Grab',
                                  siteid == 'SWROC' ~ 'Grab',
-                                 siteid == 'UBWC' & month(date) > 2 ~ 'Time Proportional',     # ISCO
+                                 siteid == 'TIDE' ~ 'Flow Proportional',
+                                 siteid == 'UBWC' & month(date) > 2 ~ 'Time Proportional',         # ISCO
                                  siteid == 'UBWC' & month(date) < 3 ~ 'Grab',
                                  siteid == 'UBWC' & month(date) == 12 & day(date) > 15 ~ 'Grab',
+                                 siteid == 'WILKIN1' ~ 'Flow Proportional',
+                                 siteid == 'WILKIN2' ~ 'Flow Proportional',
                                  siteid == 'WILKIN3' ~ 'Grab',
                                  TRUE ~ sample_type)) %>%
   # correct measurement units
@@ -45,7 +52,11 @@ wq_ALL %>%
          value = ifelse(value == 'NV', NA_character_, value),          # at WRSIS
          comments = str_replace_all(comments, 'changed to .* from', 'changed to NA from')) %>%
   select(siteid, plotid, location, height, date, time, sample_type, var_NEW, value, comments) %>%   
-  ungroup()
+  ungroup() -> temp
 
+
+temp %>%
+  filter(is.na(date)) %>%
+  distinct(siteid)
 
 

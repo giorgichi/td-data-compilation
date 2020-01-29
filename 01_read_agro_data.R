d6a4903a929827c146c4ac7d14bc3e4ecb09b31d
IASTATE\gio <giorgichi@gmail.com>
2020-01-29 15:47:21 -0600

    Read agronomic data from problematic sites
     - Added agronomic data for ACRE and standardized its IDs
     - Updated HICKS_B data with higher granularity (subplot level) for 2012-2015
     - Added dates of Forage harvest at MUDS3_OLD, which was originally incorporated in the column names
     - Added dates for Crop Development Stage at STORY
     - Added agronomic data for SWROC, including LAI with its dates
     - Removed WILKIN3 as there is not agronomic data
    
    Resolves: #isudatateam/datateam/issues/173
              #isudatateam/datateam/issues/176
    Related to: #isudatateam/datateam/issues/175

--- 01_read_agro_data.R
+++ 01_read_agro_data.R
@@ -20,7 +20,13 @@ for (i in sheets) {
 
 
 # ACRE --------------------------------------------------------------------
-ReadExcelSheets('Input_Data/AGR/ACRE Crop Yield Data.xlsx')
+ReadExcelSheets('Input_Data/AGR/ACRE Crop Yield Data.xlsx') %>%
+  bind_rows() %>%
+  rename(year = sheet, plotid = `Plot ID`) %>%
+  mutate(location = ifelse(is.na(`Sub Field`), Field, paste(`Field`, `Sub Field`, sep = '-'))) %>%
+  select(plotid, location, harvested_area = `Harvested area`, starts_with('AGR')) %>%
+  gather(key, value, starts_with('AGR')) %>%
+  filter(is.na(harvested_area)) -> agr_ACRE
 
 
 # AUGLA -------------------------------------------------------------------
@@ -122,7 +128,9 @@ ReadExcelSheets('Input_Data/AGR/HENRY Crop Yield Data.xlsx') %>%
 ReadExcelSheets('Input_Data/AGR/HICKS_B Crop Yield Data.xlsx') %>%
   bind_rows() %>%
   rename(year = sheet, plotid = `Plot ID`) %>%
-  gather(key, value, -plotid, -year) -> agr_HICKS_B
+  mutate(location = parse_number(plotid) %>% as.character(),
+         plotid = ifelse(is.na(location), plotid, str_sub(plotid, 1, 2))) %>%
+  gather(key, value, -plotid, -location, -year) -> agr_HICKS_B
 
 
 # MUDS1 -------------------------------------------------------------------
@@ -151,7 +159,13 @@ ReadExcelSheets('Input_Data/AGR/MUDS3_OLD Crop Yield Data.xlsx') %>%
   bind_rows() %>%
   rename(year = sheet, plotid = `Plot ID`) %>%
   mutate(plotid = as.character(plotid)) %>%
-  gather(key, value, -plotid, -year) -> agr_MUDS3_OLD
+  gather(key, value, -plotid, -year) %>%
+  # extract date from the variable names
+  mutate(date = word(key, -1) %>% parse_date(format = '%m/%d/%Y')) %>%
+  # get rid of dates from variable names
+  mutate(key = str_replace(key, " (\\/?[[0-9]]*){3}$", "")) %>%
+  filter(!is.na(value)) %>%
+  select(plotid, year, date, key, value) -> agr_MUDS3_OLD
 
 
 # MUDS4 -------------------------------------------------------------------
@@ -186,15 +200,44 @@ ReadExcelSheets('Input_Data/AGR/STJOHNS Crop Yield Data.xlsx') %>%
 # STORY -------------------------------------------------------------------
 ReadExcelSheets('Input_Data/AGR/STORY Crop Yield Data.xlsx') %>%
   bind_rows() %>%
-  select(-contains('Development')) %>%
   rename(year = sheet, plotid = `Plot ID`) %>%
   filter(plotid %in% c(2, 3, 5, 8, 9, 11)) %>%
   mutate(plotid = as.character(plotid)) %>%
-  gather(key, value, -plotid, -year) -> agr_STORY
+  gather(key, value, -plotid, -year) %>%
+  # extract date from the variable names
+  mutate(date = ifelse(str_detect(key, 'AGR55'), word(key, -1), NA), 
+         date = parse_date(date, format = '%Y-%m-%d')) %>% 
+  # get rid of dates from variable names
+  mutate(key = str_replace(key, " \\(weekly\\) (\\-?[[0-9]]*){3}$", "")) %>%
+  filter(!is.na(value)) %>%
+  select(plotid, year, date, key, value) -> agr_STORY
 
 
 # SWROC -------------------------------------------------------------------
-ReadExcelSheets('Input_Data/AGR/SWROC Crop Yield Data.xlsx') 
+ReadExcelSheets('Input_Data/AGR/SWROC Crop Yield Data.xlsx') %>%
+  bind_rows() %>%
+  mutate(plotid = `Plot ID`,
+         location = paste(parse_number(`Subplot ID`), 'N'),
+         year = sheet) %>%
+  gather(key, value, starts_with('AGR')) %>%
+  # remove variables with unresolved issues
+  filter(!(str_detect(key, 'AGR4 |AGRXX |AGRYY |AGRZZ '))) %>%
+  select(plotid:value) %>%
+  # there were no N treatments in 2015, hense soybean plots in 2016 did not have carry-over effect
+  mutate(location = ifelse(year == 2016 & str_detect(key, 'Soybean'), 
+                           NA_character_,
+                           location)) %>%
+  filter(!(year == 2016 & is.na(value))) -> temp_SWROC
+
+read_excel('Input_Data/AGR/SWROC LAI.xlsx', sheet = '2016', skip = 2,
+           col_names = c('crop', 'plotid', 'N_trt', 'date', 'value')) %>%
+  mutate(N_trt = as.integer(N_trt),
+         location = ifelse(is.na(N_trt), NA_character_, paste(N_trt, 'N')),
+         year = as.character(year(date)),
+         date = as.Date(date),
+         key = 'AGR56 Leaf Area Index (LAI)') %>%
+  select(plotid, location, year, date, crop, key, value) %>%
+  bind_rows(temp_SWROC) -> agr_SWROC
 
 
 # TIDE --------------------------------------------------------------------
@@ -227,10 +270,7 @@ ReadExcelSheets('Input_Data/AGR/WILKIN2 Crop Yield Data.xlsx') %>%
 
 
 # WILKIN3 -----------------------------------------------------------------
-ReadExcelSheets('Input_Data/AGR/WILKIN3 Crop Yield Data.xlsx') %>%
-  bind_rows() %>%
-  rename(year = sheet, plotid = `Plot ID`) %>%
-  gather(key, value, -plotid, -year) -> agr_WILKIN3
+ReadExcelSheets('Input_Data/AGR/WILKIN3 Crop Yield Data.xlsx')
 
 
 

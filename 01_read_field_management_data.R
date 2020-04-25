5b68e234c8154b2d7c49c268b54200779d4f4285
Giorgi Chighladze <giorgichi@gmail.com>
2020-04-24 22:58:48 -0500

    Read and standardize field mngt data

---
+++ 01_read_field_management_data.R
@@ -0,0 +1,226 @@
+# Initialize functions 
+source('00_project_settings.R')
+
+
+
+# functions for rectifying backend data
+backend_date <- function(df, COL = 'date') {
+  df %>%
+    mutate(DATE = as.numeric(!!sym(COL))) %>%
+    mutate(DATE = as.Date(DATE, origin = ymd(18991230))) %>%
+    mutate(!!COL := DATE) %>% 
+    mutate(sheet = NA, editedby = NA, updated = NA) %>%
+    select(-sheet, -editedby, -updated, -DATE)
+}
+
+plotid_expansion <- function(df, COL = 'plotid') {
+  df %>%
+    mutate(PLOT = str_split(!!sym(COL), ", ?+")) %>%
+    unnest(PLOT) %>%
+    mutate(PLOT = str_remove(PLOT, '\r$')) %>%
+    mutate(!!COL := PLOT) %>%
+    select(-PLOT)
+}
+
+
+# DOWNLOAD ................................................................
+ 
+DownloadGoogleSheet('Management DataStore (TD backend for mandata)', FOLDER = 'Metadata')
+
+
+
+# READ ....................................................................
+
+ReadExcelSheets('Input_Data/Metadata/Management DataStore (TD backend for mandata).xlsx') ->
+  fm_backend
+
+
+
+# Format data -------------------------------------------------------------
+
+#... Planting and Harvesting -----
+fm_backend %>%
+  pluck(1) %>%
+  backend_date() %>%
+  plotid_expansion() %>%
+  select(action,
+         siteid = uniqueid,
+         plotid,
+         year_calendar = calendaryear,
+         year_crop = cropyear,
+         date,
+         operation,
+         cashcrop,
+         plant_hybrid = planthybrid,
+         plant_maturity = plantmaturity,
+         plant_maturity_GDD_F = `plantmaturity_GDD (F)`,
+         plant_maturity_GDD_source = plantmaturity_GDD_source,
+         plant_rate = plantrate,
+         plant_rate_units = plantrateunits,
+         comments) %>%
+  separate(operation, into = 'operation', sep = '_', extra = 'drop') %>%
+  mutate(operation = case_when(operation == 'harvest' ~ 'harvesting',
+                               operation == 'plant'   ~ 'planting',
+                               TRUE ~ operation)) -> fm_planting
+
+
+# ... Tillage and Fertilizer Application ----
+fm_backend %>%
+  pluck(2) %>%
+  backend_date() %>%
+  plotid_expansion() %>%
+  select(action,
+         siteid = uniqueid,
+         plotid,
+         year_calendar = calendaryear,
+         year_crop = cropyear,
+         date,
+         cashcrop,
+         operation,
+         depth,
+         lime_rate = limerate,
+         manure_source = manuresource,
+         manure_method = manuremethod,
+         manure_rate = manurerate,     
+         -manurerateunits,               
+         fertilizer_form = fertilizerform,
+         fertilizer_crop = fertilizercrop,
+         fertilizer_application_type = fertilizerapptype,
+         fertilizer_formulation = fertilizerformulation,
+         fertilizer_rate = productrate,
+         nitrogen_percent = nitrogen,
+         phosphate_percent = phosphate,
+         phosphorus_percent = phosphorus, 
+         potash_percent = potash,
+         potassium_percent = potassium,   
+         sulfur_percent = sulfur,
+         zinc_percent = zinc,
+         magnesium_percent = magnesium,
+         calcium_percent = calcium,
+         iron_percent = iron,
+         nitrogen_elem,
+         phosphorus_elem,
+         potassium_elem,
+         sulfur_elem,
+         zinc_elem,
+         magnesium_elem,
+         calcium_elem,
+         iron_elem,
+         stabilizer,
+         stabilizer_used = stabilizerused,
+         stabilizer_name = stabilizername,
+         comments) %>%
+  separate(operation, into = c('operation', 'operation_type'), sep = '_', extra = 'drop') %>%
+  mutate(operation = case_when(operation == 'sample' ~ 'sampling',
+                               operation == 'fertilizer' ~ 'fertilizing',
+                               operation == 'manure'     ~ 'fertilizing',
+                               operation == 'soiladmend' ~ 'soil amendment',
+                               TRUE ~ operation),
+         operation_type = case_when(operation == 'sampling' ~ 'soil nitrate',
+                                    TRUE ~ operation_type)) -> fm_fertilizing
+
+
+# ... Residue ----
+fm_backend %>%
+  pluck(3) %>%
+  select(action,
+         siteid = uniqueid,
+         year_calendar = calendaryear,
+         year_crop = cropyear,
+         notill,
+         comments) %>%
+  ungroup() -> fm_residue
+
+
+# ... Pesticides ----
+fm_backend %>%
+  pluck(4) %>%
+  backend_date() %>%
+  mutate(operation_type = operation,
+         operation = 'pesticide application') %>%
+  select(#action,
+    siteid = uniqueid,
+    #plotid,
+    year_calendar = calendaryear,
+    year_crop = cropyear,
+    date,
+    crop,
+    operation,
+    operation_type,
+    method,
+    timing,
+    total_rate = totalrate,
+    product1_name = product1, product1_rate = rate1, product1_form = rateunit1,
+    product2_name = product2, product2_rate = rate2, product2_form = rateunit2,
+    product3_name = product3, product3_rate = rate3, product3_form = rateunit3,
+    product4_name = product4, product4_rate = rate4, product4_form = rateunit4,
+    everything()) -> fm_pesticide
+
+
+# ... Irrigation ----
+fm_backend %>%
+  pluck(5) %>%
+  backend_date(COL = 'irr_end_date') %>%
+  backend_date(COL = 'irr_start_date') %>%
+  plotid_expansion(COL = 'irr_structure') %>%
+  mutate(plotid = ifelse(uniqueid %in% c('DEFI_R', 'VANWERT'), 
+                         plotid, 
+                         irr_structure)) %>%
+  select(action,
+         siteid = uniqueid,
+         plotid,
+         irrigation_structure = irr_structure,
+         irrigation_method = irrigationmethod,
+         year_calendar = calendaryear,
+         date_irrigation_start = irr_start_date,
+         date_irrigation_end = irr_end_date,
+         irrigation_amount = irrigationamount, 
+         comments) -> fm_irrigation
+
+
+
+# ... DWM ----
+fm_backend %>%
+  pluck(6) %>%
+  select(-(updated:sheet)) %>%
+  separate(outlet_date, into = c('date', 'time'), sep = 'T') %>%
+  mutate(date = as.Date(date),
+         time = str_sub(time, 1, 5)) %>%
+  plotid_expansion(COL = 'box_structure') %>%
+  mutate(plotid = ifelse(uniqueid == 'MUDS1', box_structure, plotid)) %>%
+  select(action,
+         siteid = uniqueid,
+         plotid,
+         control_structure = box_structure,
+         year_calendar = calendaryear,        
+         date,
+         time,
+         outlet_depth = outletdepth,
+         comments) -> fm_dwm
+
+
+
+# ... Notes ----
+fm_backend %>%
+  pluck(7) %>%
+  select(1:4) %>%
+  mutate(calendaryear = str_remove(calendaryear, "\\.0$")) %>%
+  select(action,
+         siteid = uniqueid,
+         year_calendar = calendaryear,
+         notes) -> fm_notes
+
+
+
+
+# Save Metadata -----------------------------------------------------------
+
+# save management data
+write_rds(fm_planting, 'Inter_Data/fm_planting.rds', compress = 'xz')
+write_rds(fm_fertilizing, 'Inter_Data/fm_fertilizing.rds', compress = 'xz')
+write_rds(fm_pesticide, 'Inter_Data/fm_pesticide.rds', compress = 'xz')
+write_rds(fm_residue, 'Inter_Data/fm_residue.rds', compress = 'xz')
+write_rds(fm_dwm, 'Inter_Data/fm_dwm.rds', compress = 'xz')
+write_rds(fm_irrigation, 'Inter_Data/fm_irrigation.rds', compress = 'xz')
+write_rds(fm_notes, 'Inter_Data/fm_notes.rds', compress = 'xz')
+
---
+++ 03_standardize_field_management_data.R
@@ -0,0 +1,197 @@
+# Initialize functions 
+source('00_project_settings.R')
+
+
+
+# Read All field management data -----
+planting <- read_rds('Inter_Data/fm_planting.rds')
+fertilizing <- read_rds('Inter_Data/fm_fertilizing.rds')
+pesticide <- read_rds('Inter_Data/fm_pesticide.rds')
+residue <- read_rds('Inter_Data/fm_residue.rds')
+dwm <- read_rds('Inter_Data/fm_dwm.rds')
+irrigation <- read_rds('Inter_Data/fm_irrigation.rds')
+notes <- read_rds('Inter_Data/fm_notes.rds')
+
+# Get plot and location identifiers for agronomic data
+agr_ids <- 
+  read_rds('Standard_Data/agro_ALL.rds') %>%
+  distinct(siteid, plotid , location) %>%
+  rename(PLOT = plotid) %>%
+  # plots that does not have crop yield data
+  add_row(siteid = c('DEFI_R', 'UBWC', 'UBWC'), 
+          PLOT = c('10', 'B2', 'B4'))
+
+
+
+# Function to standardize plotids at ACRE
+rename_ACRE_plots <- function(df){
+  df %>%
+    mutate(plotid = ifelse(siteid == 'ACRE' & plotid == '22E/W', '22E', plotid)) %>%
+    mutate(plotid = ifelse(siteid == 'ACRE' & str_detect(plotid, '[:alpha:]'), 
+                           str_replace(plotid, '[:alpha:]', 
+                                       paste0('-', str_extract(plotid, "[:alpha:]"))),
+                           plotid),
+           location = ifelse(siteid == 'ACRE', paste('Field', plotid), NA_character_)) %>%
+    left_join(agr_ids %>% filter(siteid == 'ACRE'),
+              by = c('siteid', 'location')) %>%
+    mutate(plotid = ifelse(siteid == 'ACRE', PLOT, plotid)) %>%
+    select(action:plotid, location, everything(), - PLOT)
+}
+
+# Function to identify additional location-years to remove at ACRE
+correct_ACRE_action <- function(df){
+  REMOVE <- read_rds('Standard_Data/agro_ALL.rds') %>%
+    filter(siteid == 'ACRE' & action == 'remove') %>%
+    mutate(year_calendar = as.numeric(year)) %>%
+    distinct(siteid, plotid, location, year_calendar, REMOVE = 'yes')
+  df %>%
+    left_join(REMOVE, by = c('siteid', 'plotid', 'location', 'year_calendar')) %>%
+    mutate(action = ifelse(!is.na(REMOVE), 'remove', action)) %>%
+    select(-REMOVE)
+  }
+
+# Function to standardize plotids at DEFI_R
+rename_DEFI_R_plots <- function(df) {
+  df %>%
+    mutate(location = ifelse(siteid == 'DEFI_R', 
+                             str_extract(plotid, '[:alpha:]'), 
+                             location),
+           plotid = ifelse(siteid == 'DEFI_R' & !is.na(location), 
+                           str_remove(plotid, '[:alpha:]'), 
+                           plotid),
+           location = case_when(siteid == 'DEFI_R' & location == 'N' ~ 'North half',
+                                siteid == 'DEFI_R' & location == 'S' ~ 'South half',
+                                TRUE ~ location))
+  }
+
+
+# Function to standardize plotids at SWROC
+rename_SWROC_plots <- function(df) {
+  df %>%
+    separate(plotid, into = c('PLOT', 'LOC'), sep = "_", remove = FALSE, convert = TRUE) %>%
+    mutate(plotid   = ifelse(siteid == 'SWROC', PLOT, plotid),
+           location = ifelse(siteid == 'SWROC', paste(LOC, 'N'), location)) %>%
+    select(-PLOT, - LOC)
+}
+
+# Function to identify non-relevant plots at STORY
+correct_STORY_action <- function(df){
+  df %>%
+    mutate(action = ifelse(siteid == 'STORY' & plotid %in% c(1, 4, 6, 7, 10, 12), 
+                           'remove', action))
+}
+
+
+# Standardize Planting Data ----------
+
+# standardize plot ids
+planting %>%
+  rename_ACRE_plots() %>%
+  rename_DEFI_R_plots() %>%
+  correct_ACRE_action() -> planting_standard
+
+
+
+# Standardize Fertilizer Data ----------
+
+# correct elemental rates
+fertilizing %>%
+  mutate(check_n = as.numeric(fertilizer_rate) * as.numeric(nitrogen_percent) / 100,
+         check_p = as.numeric(fertilizer_rate) * as.numeric(phosphate_percent) / 100 / 2.2915,
+         check_k = as.numeric(fertilizer_rate) * as.numeric(potash_percent) / 100 / 1.2047,
+         check_s = as.numeric(fertilizer_rate) * as.numeric(sulfur_percent) / 100 ,
+         check_z = as.numeric(fertilizer_rate) * as.numeric(zinc_percent) / 100,
+         check_m = as.numeric(fertilizer_rate) * as.numeric(magnesium_percent) / 100,
+         check_c = as.numeric(fertilizer_rate) * as.numeric(calcium_percent) / 100,
+         check_i = as.numeric(fertilizer_rate) * as.numeric(iron_percent) / 100) %>%
+  mutate_at(vars(starts_with('check')), round, digits = 2) %>%
+  mutate_at(vars(starts_with('check')), as.character) %>%
+  mutate(nitrogen_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_n, nitrogen_elem),
+         phosphorus_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_p, phosphorus_elem),
+         potassium_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_k, potassium_elem),
+         sulfur_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_s, sulfur_elem),
+         zinc_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_z, zinc_elem),
+         magnesium_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_m, magnesium_elem),
+         calcium_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_c, calcium_elem),
+         iron_elem = ifelse(!is.na(as.numeric(fertilizer_rate)), check_i, iron_elem)) %>%
+  select(-ends_with('percent'),-starts_with('check')) %>%
+  # variable 'stabilizer' is redundant - NEED TO ASK LORI
+  select(everything()) -> fertilizing_corrected
+
+
+# standardize plot ids
+fertilizing_corrected %>%
+  rename_ACRE_plots() %>%
+  correct_STORY_action() %>%
+  rename_SWROC_plots() %>% 
+  rename_DEFI_R_plots() %>%
+  # remove erroneous plot id and correct locations at VANWERT
+  mutate(plotid = ifelse(siteid == 'VANWERT' & plotid == '68', NA, plotid),
+         location = case_when(siteid == 'VANWERT' & plotid == 1 ~ 'SE',
+                              siteid == 'VANWERT' & plotid == 2 ~ 'SW',
+                              TRUE ~ location),
+         plotid =  case_when(siteid == 'VANWERT' & plotid == 1 ~ 'S',
+                             siteid == 'VANWERT' & plotid == 2 ~ 'S',
+                             siteid == 'VANWERT' & plotid == 3 ~ 'D',
+                             TRUE ~ plotid)) -> fertilizing_standard
+
+
+
+# Standardize Pesticide Data ----------
+
+pesticide -> pesticide_standard
+  
+
+
+# Standardize Residue Data ----------
+
+residue -> 
+  # NEED to move crop from comments to cashcrop column
+  residue_standard
+
+
+
+# Standardize DWM Data ----------
+
+dwm %>%
+  mutate(temp = round(as.numeric(outlet_depth), 2),
+         outlet_depth = ifelse(!is.na(temp), as.character(temp), outlet_depth),
+         temp = NULL) %>%
+  rename(controlled_plotids = plotid) -> dwm_standard
+
+
+# Standardize Irrigation Data ----------
+irrigation %>%
+  mutate(temp = round(as.numeric(irrigation_amount), 2),
+         irrigation_amount = ifelse(!is.na(temp), as.character(temp), irrigation_amount),
+         temp = NULL) %>%
+  rename(irrigated_plotids = plotid) -> irrigation_standard
+  
+
+
+# Standardize Notes Data ----------
+notes ->
+  notes_standard
+
+
+
+
+# Save standardized data --------------------------------------------------
+
+write_rds(dwm_standard, 'Standard_Data/dwm_ALL.rds', compress = 'xz')
+write_rds(fertilizing_standard, 'Standard_Data/fertilizing_ALL.rds', compress = 'xz')
+write_rds(irrigation_standard, 'Standard_Data/irrigation_ALL.rds', compress = 'xz')
+write_rds(notes_standard, 'Standard_Data/notes_ALL.rds', compress = 'xz')
+write_rds(planting_standard, 'Standard_Data/planting_ALL.rds', compress = 'xz')
+
+
+
+#########################
+##### CHEK PLOT IDs #####
+#########################
+dwm %>%
+  filter(!is.na(plotid)) %>%
+  anti_join(agr_ids, by = c('siteid', 'plotid' = 'PLOT')) %>%
+  distinct(action, siteid, plotid) %>%
+  View()
+
